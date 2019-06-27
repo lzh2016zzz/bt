184a63dae51bdfa3fd8ed71b7308c1f19feae7b8
laizehao <whatalow@foxmail.com>
2019-06-27 17:26:04 +0800

    修改配置

--- dht-server/src/main/java/com/lzh/dhtserver/logic/DHTServer.java
+++ dht-server/src/main/java/com/lzh/dhtserver/logic/DHTServer.java
@@ -58,7 +58,7 @@ public class DHTServer {
     public void start() throws Exception {
         log.info("启动dht-server,udpPort :{}  ", udpPort);
         serverChannelFuture = b.bind(udpPort).sync();
-        serverChannelFuture.channel().closeFuture();
+        serverChannelFuture.channel().closeFuture().sync();
         log.info("启动dht-server成功.");
     }
 
