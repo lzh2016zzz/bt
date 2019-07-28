e166cdf8da7bd554fcd010742b3de4fa94524a0b
laizh <whatalow@foxmail.com>
2019-07-28 14:32:43 +0800

    部分实体数组直接赋值变成拷贝数组

--- dht-server/src/main/java/com/lzh/dhtserver/common/entity/DownloadMsgInfo.java
+++ dht-server/src/main/java/com/lzh/dhtserver/common/entity/DownloadMsgInfo.java
@@ -4,6 +4,7 @@ import com.lzh.dhtserver.common.util.SystemClock;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /***
  * Peer Wire 下载消息信息
@@ -11,15 +12,30 @@ import java.io.Serializable;
 @Data
 public class DownloadMsgInfo implements Serializable {
 
-	private String ip;
-	private int port;
-	private byte[] infoHash;
-	private long timestamp;
+    private String ip;
+    private int port;
+    private byte[] infoHash;
+    private long timestamp;
 
-	public DownloadMsgInfo(String ip, int port, byte[] infoHash) {
-		this.ip = ip;
-		this.port = port;
-		this.infoHash = infoHash;
-		timestamp = SystemClock.now();
-	}
+    public DownloadMsgInfo(String ip, int port, byte[] infoHash) {
+        this.ip = ip;
+        this.port = port;
+        if (infoHash != null) {
+            this.infoHash = Arrays.copyOf(infoHash, infoHash.length);
+        }
+        timestamp = SystemClock.now();
+    }
+
+    public void setInfoHash(byte[] infoHash) {
+        if (infoHash == null) {
+            this.infoHash = null;
+        } else this.infoHash = Arrays.copyOf(infoHash, infoHash.length);
+    }
+
+    public byte[] getInfoHash() {
+        if (this.infoHash == null) {
+            return null;
+        }
+        return Arrays.copyOf(this.infoHash, this.infoHash.length);
+    }
 }
--- dht-server/src/main/java/com/lzh/dhtserver/logic/entity/Node.java
+++ dht-server/src/main/java/com/lzh/dhtserver/logic/entity/Node.java
@@ -3,6 +3,7 @@ package com.lzh.dhtserver.logic.entity;
 import lombok.Data;
 
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 
 /***
  * DHT 节点
@@ -11,11 +12,26 @@ import java.net.InetSocketAddress;
 @Data
 public class Node {
 
-	private byte[] nodeId;
-	private InetSocketAddress addr;
+    private byte[] nodeId;
+    private InetSocketAddress addr;
 
-	public Node(byte[] nodeId, InetSocketAddress addr) {
-		this.nodeId = nodeId;
-		this.addr = addr;
-	}
+    public Node(byte[] nodeId, InetSocketAddress addr) {
+        if (nodeId != null) {
+            this.nodeId = Arrays.copyOf(nodeId, nodeId.length);
+        }
+        this.addr = addr;
+    }
+
+    public void setNodeId(byte[] nodeId) {
+        if (nodeId == null) {
+            this.nodeId = null;
+        } else this.nodeId = Arrays.copyOf(nodeId, nodeId.length);
+    }
+
+    public byte[] getNodeId() {
+        if (this.nodeId == null) {
+            return null;
+        }
+        return Arrays.copyOf(this.nodeId, this.nodeId.length);
+    }
 }
